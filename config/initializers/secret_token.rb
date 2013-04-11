5e85aa748e6ebcd417f219916f82c3aa5813275f
Steven Harman <steveharman@gmail.com>
2013-04-11 18:00:17 -0400

    rails new debug_info -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DebugInfo::Application.config.secret_key_base = 'b5e7918f19315a9f6e54056dbc46b8cae2913e2af9f025020d09ddbc769dba0e32808887d55f0dc3829cc618086507f803ab8ec487ca70a238d6d51ef633d4c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DebugInfo::Application.config.session_store :encrypted_cookie_store, key: '_debug_info_session'
